47b81c61b1d41b06a6a433f9cea1a8827e99582c
Kevin de Oliveira <contact@kevindeoliveira.com>
2022-03-26 13:33:01 -0400

    qt gui for qnx

---
+++ gui/inputdata.h
@@ -0,0 +1,17 @@
+#ifndef INPUTDATA_H
+#define INPUTDATA_H
+
+#include <QObject>
+
+class InputData : public QObject
+{
+    Q_OBJECT
+public:
+    explicit InputData(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // INPUTDATA_H
